6d238e509cafebcacc9fc3920d7ca7b47918cfc3
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-04-16 11:54:04 -0700

    attempt a slightly more reasonable display (still a bit broken though)

--- uxhell/tui/form.go
+++ uxhell/tui/form.go
@@ -12,71 +12,93 @@ func newForm() *huh.Form {
 	var lettercase string
 	return huh.NewForm(
 		huh.NewGroup(
-			huh.NewConfirm().
-				Title("Are you done yet?").
-				Affirmative("!No").
-				Negative("!Yes").
-				Value(&done),
-			huh.NewSelect[string]().
-				Title("Do you want a letter or punctuation?").
-				Options(
-					huh.NewOption("letter", "letter"),
-					huh.NewOption("punctuation", "punctuation"),
-				).
-				Value(&want),
-			huh.NewSelect[string]().
-				TitleFunc(func() string {
-					switch want {
-					case "letter":
-						return "Do you want an uppercase or lowercase letter?"
-					default:
-						return "This is a filler, just hit continue."
-					}
-				}, &want).
-				OptionsFunc(func() []huh.Option[string] {
-					switch want {
-					case "letter":
-						return huh.NewOptions("UPPERCASE", "lowercase")
-					default:
-						return huh.NewOptions("continue", "Continue")
-					}
-				}, &want).
-				Validate(func(s string) error {
-					switch want {
-					case "letter":
-						return nil
-					default:
-						if s == "Continue" {
-							return errors.New("no, I said to hit 'continue', not 'Continue'")
-						}
-						return nil
-					}
-				}).
-				Value(&lettercase),
-			huh.NewSelect[string]().
-				TitleFunc(func() string {
-					switch lettercase {
-					case "UPPERCASE":
-						return "PICK YOUR LETTER!"
-					case "lowercase":
-						return "pick your letter."
-					default:
-						return "Pick your punctuation."
-					}
-				}, &lettercase).
-				OptionsFunc(func() []huh.Option[string] {
-					switch lettercase {
-					case "UPPERCASE":
-						return options(letters)
-					case "lowercase":
-						return options(strings.ToLower(letters))
-					default:
-						return options(punctuation)
-					}
-				}, &lettercase).
-				Key("input"),
+			letterOrPunctuationSelect(&want),
+		),
+		huh.NewGroup(
+			lettercaseSelect(&want, &lettercase),
+		),
+		huh.NewGroup(
+			charSelect(&lettercase),
+		),
+		huh.NewGroup(
+			doneConfirm(&done),
 		),
 	)
+}
+
+func charSelect(lettercase *string) *huh.Select[string] {
+	return huh.NewSelect[string]().
+		TitleFunc(func() string {
+			switch *lettercase {
+			case "UPPERCASE":
+				return "PICK YOUR LETTER!"
+			case "lowercase":
+				return "pick your letter."
+			default:
+				return "Pick your punctuation."
+			}
+		}, lettercase).
+		OptionsFunc(func() []huh.Option[string] {
+			switch *lettercase {
+			case "UPPERCASE":
+				return options(letters)
+			case "lowercase":
+				return options(strings.ToLower(letters))
+			default:
+				return options(punctuation)
+			}
+		}, lettercase).
+		Key("input")
+}
+
+func lettercaseSelect(want *string, lettercase *string) *huh.Select[string] {
+	return huh.NewSelect[string]().
+		TitleFunc(func() string {
+			switch *want {
+			case "letter":
+				return "Do you want an uppercase or lowercase letter?"
+			default:
+				return "This is a filler, just hit continue."
+			}
+		}, want).
+		OptionsFunc(func() []huh.Option[string] {
+			switch *want {
+			case "letter":
+				return huh.NewOptions("UPPERCASE", "lowercase")
+			default:
+				return huh.NewOptions("continue", "Continue")
+			}
+		}, want).
+		Validate(func(s string) error {
+			switch *want {
+			case "letter":
+				return nil
+			default:
+				if s == "Continue" {
+					return errors.New("no, I said to hit 'continue', not 'Continue'")
+				}
+				return nil
+			}
+		}).
+		Value(lettercase)
+}
+
+func letterOrPunctuationSelect(want *string) *huh.Select[string] {
+	return huh.NewSelect[string]().
+		Title("Do you want a letter or punctuation?").
+		Options(
+			huh.NewOption("letter", "letter"),
+			huh.NewOption("punctuation", "punctuation"),
+		).
+		Value(want)
+}
+
+func doneConfirm(done *bool) *huh.Confirm {
+	return huh.NewConfirm().
+		Title("Are you done yet?").
+		Affirmative("!No").
+		Negative("!Yes").
+		Value(done)
 }
 
 func options(from string) []huh.Option[string] {
--- uxhell/tui/model.go
+++ uxhell/tui/model.go
@@ -2,9 +2,11 @@ package tui
 
 import (
 	"fmt"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/lipgloss"
 )
 
 const (
@@ -31,23 +33,39 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.form = m.form.WithHeight(msg.Height - 2)
+		m.form = m.form.WithWidth(msg.Width)
 	case tea.KeyMsg:
 		switch msg.Type {
 		case tea.KeyCtrlC:
 			return m, tea.Quit
 		}
 	}
+
 	form, cmd := m.form.Update(msg)
 	if f, ok := form.(*huh.Form); ok {
 		m.form = f
+	}
+
+	if m.form.State == huh.StateCompleted {
+		m.input += m.form.GetString("input")
+		if !done {
+			m.form = newForm()
+		}
 	}
 
 	return m, cmd
 }
 
 func (m model) View() string {
-	if m.form.State == huh.StateCompleted {
-		return fmt.Sprintf("you chose %s", m.form.GetString("input"))
+	if m.form.State == huh.StateCompleted && done {
+		return fmt.Sprintf("you wrote %s", m.input)
 	}
-	return m.form.View()
+	msg := "You haven't written anything.\n"
+	if m.input != "" {
+		msg = fmt.Sprintf("You've written: %s\n", m.input)
+	}
+	form := lipgloss.NewStyle().Render(strings.TrimSuffix(m.form.View(), "\n\n"))
+	return msg + form
 }
